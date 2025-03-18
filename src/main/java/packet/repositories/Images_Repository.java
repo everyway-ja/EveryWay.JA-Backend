9fdb3aa45045767c2ce8f31f85ac89e3cf946382
LorenBll <lorenzo.bellotta@hotmail.com>
2025-03-18 20:49:42 +0100

    Created facade and introduced images' model and repository.

---
+++ src/main/java/packet/repositories/Images_Repository.java
@@ -0,0 +1,17 @@
+package packet.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import packet.model.tables.Image;
+import java.util.List;
+
+@Repository
+
+public interface Image_Repository extends JpaRepository<Image, Integer> {
+
+    Image findById(int id);
+
+    List<Image> findByImage(String image);
+    List<Image> findByDescription(String description);
+
+}
--- src/main/java/packet/services/Facade.java
+++ src/main/java/packet/services/Facade.java
@@ -2,11 +2,13 @@ package packet.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import packet.services.actual_services.*;
 
 @Service @RequiredArgsConstructor
 
 public class Facade {
 
-    
+    // TODO : implement the actual services, and then instantiate them here. after this, implement facade methods.
+    // TODO : the suggestion algorithm should also be implemented.
 
 }
