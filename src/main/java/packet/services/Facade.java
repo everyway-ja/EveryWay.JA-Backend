5367715d1d4b641dce6b0f2e23027c28333aabce
LoWolfo <125542958+LWolfo@users.noreply.github.com>
2025-03-19 12:00:07 +0100

    added unit test

--- src/main/java/packet/services/Facade.java
+++ src/main/java/packet/services/Facade.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Service;
 @Service @RequiredArgsConstructor
 
 public class Facade {
+    
+    public String ciao() {
+        return "ciao";
+    }
 
     // TODO : implement the actual services, and then instantiate them here. after this, implement facade methods.
     // TODO : the suggestion algorithm should also be implemented.
