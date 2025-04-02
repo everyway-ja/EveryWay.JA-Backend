cdc93deedcd7d899ffe120fbdc799dd3036815f8
LorenBll <lorenzo.bellotta@hotmail.com>
2025-04-02 11:06:42 +0200

    Removed python files and implemented "@Autowired" in the controllers.

--- src/main/java/everyway/everyway/controllers/FunctionalitiesController.java
+++ src/main/java/everyway/everyway/controllers/FunctionalitiesController.java
@@ -11,33 +11,34 @@ import java.time.LocalDate;
 import java.util.List;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 
 public class FunctionalitiesController {
 
-    private final AccountCategories_Accounts_Service accountCategories_Accounts_Service = new AccountCategories_Accounts_Service();
-    private final AccountCategories_Itineraries_Service accountCategories_Itineraries_Service = new AccountCategories_Itineraries_Service();
-    private final AccountCategories_LocationReports_Service accountCategories_LocationReports_Service = new AccountCategories_LocationReports_Service();
-    private final AccountCategories_Locations_Service accountCategories_Locations_Service = new AccountCategories_Locations_Service();
-    private final AccountCategories_Service accountCategories_Service = new AccountCategories_Service();
-    private final Accounts_Positions_Service accounts_Positions_Service = new Accounts_Positions_Service();
-    private final Accounts_Service accounts_Service = new Accounts_Service();
-    private final Images_Service images_Service = new Images_Service();
-    private final Images_Itineraries_Service images_Itineraries_Service = new Images_Itineraries_Service();
-    private final Images_Locations_Service images_Locations_Service = new Images_Locations_Service();
-    private final Itineraries_Service itineraries_Service = new Itineraries_Service();
-    private final ItineraryCategories_Service itineraryCategories_Service = new ItineraryCategories_Service();
-    private final ItineraryCategories_Itineraries_Service itineraryCategories_Itineraries_Service = new ItineraryCategories_Itineraries_Service();
-    private final Languages_Service languages_Service = new Languages_Service();
-    private final LocationCategories_Service locationCategories_Service = new LocationCategories_Service();
-    private final LocationCategories_LocationReports_Service locationCategories_LocationReports_Service = new LocationCategories_LocationReports_Service();
-    private final LocationCategories_Locations_Service locationCategories_Locations_Service = new LocationCategories_Locations_Service();
-    private final LocationReports_Service locationReports_Service = new LocationReports_Service();
-    private final Locations_Service locations_Service = new Locations_Service();
-    private final Locations_Itineraries_Accounts_Service locations_Itineraries_Accounts_Service = new Locations_Itineraries_Accounts_Service();
-    private final Positions_Service positions_Service = new Positions_Service();
-    private final Reviews_Service reviews_Service = new Reviews_Service();
+    @Autowired private AccountCategories_Accounts_Service accountCategories_Accounts_Service;
+    @Autowired private AccountCategories_Itineraries_Service accountCategories_Itineraries_Service;
+    @Autowired private AccountCategories_LocationReports_Service accountCategories_LocationReports_Service;
+    @Autowired private AccountCategories_Locations_Service accountCategories_Locations_Service;
+    @Autowired private AccountCategories_Service accountCategories_Service;
+    @Autowired private Accounts_Positions_Service accounts_Positions_Service;
+    @Autowired private Accounts_Service accounts_Service;
+    @Autowired private Images_Service images_Service;
+    @Autowired private Images_Itineraries_Service images_Itineraries_Service;
+    @Autowired private Images_Locations_Service images_Locations_Service;
+    @Autowired private Itineraries_Service itineraries_Service;
+    @Autowired private ItineraryCategories_Service itineraryCategories_Service;
+    @Autowired private ItineraryCategories_Itineraries_Service itineraryCategories_Itineraries_Service;
+    @Autowired private Languages_Service languages_Service;
+    @Autowired private LocationCategories_Service locationCategories_Service;
+    @Autowired private LocationCategories_LocationReports_Service locationCategories_LocationReports_Service;
+    @Autowired private LocationCategories_Locations_Service locationCategories_Locations_Service;
+    @Autowired private LocationReports_Service locationReports_Service;
+    @Autowired private Locations_Service locations_Service;
+    @Autowired private Locations_Itineraries_Accounts_Service locations_Itineraries_Accounts_Service;
+    @Autowired private Positions_Service positions_Service;
+    @Autowired private Reviews_Service reviews_Service;
     
 
 
